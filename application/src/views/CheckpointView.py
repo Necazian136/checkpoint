9ac4d9361ea68e4b187233659b51cf189a19622b
Necazian <yur1999@mail.ru>
2019-01-12 23:32:17 +0400

    [update] Plates now can't been seen from main page

--- application/src/views/CheckpointView.py
+++ application/src/views/CheckpointView.py
@@ -16,10 +16,10 @@ class CheckpointView(View):
             user = request.user
             try:
                 checkpoint = Checkpoint.objects.get(user=user)
-            # Check if checkpoint is created
+            # check if checkpoint is created
             except ObjectDoesNotExist:
                 return render(request, "main/creation.html", {'form': form})
-            # Check if checkpoint is created
+            # check if checkpoint is created
             if user is not None and user.is_active:
                 return render(request, "main/index.html", {'checkpoint': checkpoint})
         return redirect('login/')
