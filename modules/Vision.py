92c74065ef9c50e2d88628c890cac1535aed89ee
Necazian <yur1999@mail.ru>
2019-01-12 11:31:56 +0400

    [add] stream img saves in file for plate recognition

--- modules/Vision.py
+++ modules/Vision.py
@@ -6,12 +6,14 @@ class Vision:
         self.cap = cv2.VideoCapture(0)
         self.frame = None
         self.buf = None
+        self.filepath = 'img.jpg'
         import threading
         t = threading.Thread(target=self.stream, args=())
         t.start()
 
     def stream(self):
-        if self.cap is not None:
-            ret, self.frame = self.cap.read()
-            r, self.buf = cv2.imencode(".jpg", self.frame)
-        self.stream()
+        while True:
+            if self.cap is not None:
+                ret, self.frame = self.cap.read()
+                r, self.buf = cv2.imencode(".jpg", self.frame)
+                cv2.imwrite(self.filepath, self.frame)
